940a3da552df529e07a53c3d1b2ca9026c95a1e4
Lionel Tan <lionel_tan@tech.gov.sg>
2024-11-08 10:00:33 +0800

    Fix port issue

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Array of 5 random strings
 const randomTexts = [
